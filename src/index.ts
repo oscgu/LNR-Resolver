27b9bdae8a1a12c0f2bddebab6ba58321a461529
oscgu <oscarguertler@gmail.com>
2022-11-11 12:38:46 +0000

    export lnrConfig type

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { LnrConfigProvider } from "./provider/LnrConfigProvider";
 export { useLnrGetName } from "./hooks/useLnrGetName";
 export { useLnrGetAddress } from "./hooks/useLnrGetAddress";
+export { LnrConfig } from "./provider/LnrConfig";
